01d7ea7d308afc9afebee67190e034ab0ddc72a2
Michael <michaeloesterle01@gmail.com>
2024-10-13 22:49:59 +0200

    Build as PWA

---
+++ docs/assets/settings.page.6b7aa5bc.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.a3139774.js";import{L as a,M as s,O as n,$ as r}from"./index.19a7f53a.js";const m=Object.assign({name:"SettingsPage"},{__name:"settings.page",setup(o){return(i,t)=>(a(),s(e,null,{default:n(()=>t[0]||(t[0]=[r("div",{class:"text-h6 q-pa-md text-center"},"Settings",-1)])),_:1}))}});export{m as default};
